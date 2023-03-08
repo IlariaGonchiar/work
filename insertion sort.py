aa3c285ee4bfd51fdba8cd968fbe9c0a576957af
IlariaGonchiar <125513828+IlariaGonchiar@users.noreply.github.com>
2023-03-08 23:56:48 +0200

    Add files via upload

---
+++ insertion sort.py
@@ -0,0 +1,13 @@
+def insertion_sort(arr):
+
+    for i in range(len(arr)):
+        cursor = arr[i]
+        pos = 1
+
+        while pos > 0 and arr[pos - 1] > cursor:
+            arr[pos] = arr[pos - 1]
+            pos = pos - 1
+
+        arr[pos] = cursor
+
+    return arr
